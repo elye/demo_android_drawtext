e0b2496301b70977dd106ee18f0bcae58385aee5
elye <elye.project@gmail.com>
2019-07-30 19:46:26 +1000

    Initial commit

---
+++ app/src/main/java/com/elyeproj/drawtext/DrawTextView.kt
@@ -0,0 +1,169 @@
+package com.elyeproj.drawtext
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import java.lang.Math.abs
+
+class DrawTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : View(context, attrs, defStyleAttr, defStyleRes) {
+
+    companion object {
+        private const val TEXT = "Sample Text"
+    }
+
+    private var drawText = TEXT
+    private val drawTextCoordinate = Coordinate()
+
+    var customText: String = ""
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var drawBox: Boolean = false
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    // This is to set to ensure the coordinate Y position is fix to the
+    // Sample Text height, so that it doesn't move despite of the drawText height change
+    // This make it prettier when perform drawing, so the text stay on the baseline regardless
+    // of the drawText change height.
+    var fixHeightCoordinate: Boolean = false
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var customCenter: Boolean = false
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var customAlign: Paint.Align = Paint.Align.CENTER
+        set(value) {
+            field = value
+            projectResources.paintText.textAlign = field
+            invalidate()
+        }
+
+    var customStyle: Paint.Style = Paint.Style.FILL
+        set(value) {
+            field = value
+            projectResources.paintText.style = field
+            invalidate()
+        }
+
+    private val sampleTextBound by lazy {
+        val textBound = Rect()
+        projectResources.paintText.getTextBounds(TEXT, 0, TEXT.length, textBound)
+        textBound
+    }
+
+    private var originTextBound = calculateOriginTextBound()
+    private var translatedTextBound = calculateDrawTextBound()
+
+    private fun calculateOriginTextBound(): Rect {
+        val textBound = Rect()
+        projectResources.paintText.getTextBounds(drawText, 0, drawText.length, textBound)
+        return textBound
+    }
+
+    private fun calculateDrawTextBound(): RectF {
+        val drawTextBound = RectF()
+        if (customCenter) {
+            drawTextBound.top = height / 2f - originTextBound.calculateCenterY()
+            drawTextBound.bottom = height / 2f + originTextBound.calculateCenterY()
+        } else {
+            drawTextBound.top = height / 2f - originTextBound.calculateCenterY() + originTextBound.bottom
+            drawTextBound.bottom = height / 2f + originTextBound.calculateCenterY() + originTextBound.bottom
+        }
+
+        when(customAlign) {
+            Paint.Align.LEFT -> {
+                drawTextBound.left = width / 2f + originTextBound.right
+                drawTextBound.right = width / 2f + originTextBound.left
+            }
+            Paint.Align.CENTER -> {
+                drawTextBound.left = width / 2f - originTextBound.calculateCenterX()
+                drawTextBound.right = width / 2f + originTextBound.calculateCenterX()
+            }
+            Paint.Align.RIGHT -> {
+                drawTextBound.left = width / 2f - originTextBound.right
+                drawTextBound.right = width / 2f + originTextBound.left
+            }
+        }
+        return drawTextBound
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        if (width == 0 || height == 0) return
+
+        drawText = if (customText.isBlank()) TEXT else customText
+        originTextBound = calculateOriginTextBound()
+        translatedTextBound = calculateDrawTextBound()
+
+        drawTextCoordinate.x = width / 2f
+
+        if (fixHeightCoordinate) {
+            if (customCenter) {
+                drawTextCoordinate.y = height / 2f - sampleTextBound.exactCenterY()
+            } else {
+                drawTextCoordinate.y = height / 2f + sampleTextBound.calculateCenterY()
+            }
+        } else {
+            if (customCenter) {
+                drawTextCoordinate.y = height / 2f - originTextBound.exactCenterY()
+            } else {
+                drawTextCoordinate.y = height / 2f + originTextBound.calculateCenterY()
+            }
+
+        }
+
+        canvas.drawText(drawText, drawTextCoordinate.x, drawTextCoordinate.y, projectResources.paintText)
+        canvas.drawPoint(drawTextCoordinate.x, drawTextCoordinate.y, projectResources.paintRed)
+        if (drawBox) {
+            canvas.drawRect(translatedTextBound, projectResources.paintBox)
+        }
+
+
+        //////////////////////////////////////////////////////////////////////////////
+        // Fix the draw line per the full height of the text using the Sample Text  //
+        //////////////////////////////////////////////////////////////////////////////
+        canvas.drawLine(0f, 0f, width.toFloat(), 0f, projectResources.paintLight)
+        canvas.drawLine(0f, height / 2f, width.toFloat(), height / 2f, projectResources.paintLight)
+        canvas.drawLine(
+            0f, height / 2f - sampleTextBound.calculateCenterY(),
+            width.toFloat(),
+            height / 2f - sampleTextBound.calculateCenterY(),
+            projectResources.paintLight
+        )
+        canvas.drawLine(
+            0f,
+            height / 2f + sampleTextBound.calculateCenterY(),
+            width.toFloat(),
+            height / 2f + sampleTextBound.calculateCenterY(),
+            projectResources.paintLight
+        )
+        canvas.drawLine(0f, height.toFloat(), width.toFloat(), height.toFloat(), projectResources.paintLight)
+    }
+
+    private fun Rect.calculateCenterY(): Float {
+        return abs((bottom - top) / 2f)
+    }
+
+    private fun Rect.calculateCenterX(): Float {
+        return abs((right - left) / 2f)
+    }
+
+    class Coordinate(var x: Float = 0f, var y: Float = 0f)
+}
---
+++ app/src/main/java/com/elyeproj/drawtext/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.elyeproj.drawtext
+
+import android.graphics.Paint
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        projectResources = ProjectResources(resources)
+        setContentView(R.layout.activity_main)
+
+        text_input.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                draw_text_view.customText = text_input.text.toString()
+            }
+        })
+
+        switch_box.setOnCheckedChangeListener { _, isChecked ->
+            draw_text_view.drawBox = isChecked
+        }
+
+        switch_fix_height_coordinate.setOnCheckedChangeListener { _, isChecked ->
+            draw_text_view.fixHeightCoordinate = isChecked
+        }
+
+        (radio_center.getChildAt(0) as RadioButton).isChecked = true
+        radio_center.setOnCheckedChangeListener { _, checkedId ->
+            draw_text_view.customCenter = (checkedId == 2)
+        }
+
+        (radio_align.getChildAt(0) as RadioButton).isChecked = true
+        radio_align.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId) {
+                3 -> draw_text_view.customAlign = Paint.Align.CENTER
+                4 -> draw_text_view.customAlign = Paint.Align.LEFT
+                5 -> draw_text_view.customAlign = Paint.Align.RIGHT
+            }
+        }
+
+        (radio_style.getChildAt(0) as RadioButton).isChecked = true
+        radio_style.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId) {
+                6 -> draw_text_view.customStyle = Paint.Style.FILL
+                7 -> draw_text_view.customStyle = Paint.Style.FILL_AND_STROKE
+                8 -> draw_text_view.customStyle = Paint.Style.STROKE
+            }
+        }
+
+    }
+}
